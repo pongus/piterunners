3194e8c633e0706f300239289e1b9ebff353aabf
Pontus Gustafsson <pontus.gustafsson@datscha.com>
2021-12-10 17:19:30 +0100

    Corrected api url

--- client/src/apis/RestaurantFinder.js
+++ client/src/apis/RestaurantFinder.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-import axiosRetry from "axios-retry";
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
 
 const RestaurantFinder = axios.create({
   baseURL:
-    process.env.NODE_ENV !== "production"
-      ? "http://localhost:7000/api/v1/restaurants"
-      : "https://alluring-bryce-canyon-75245.herokuapp.com/api/v1/restaurants",
+    process.env.NODE_ENV !== 'production'
+      ? 'http://localhost:7000/api/v1/restaurants'
+      : 'https://piterunners.herokuapp.com/api/v1/restaurants',
   timeout: 5000,
 });
 
