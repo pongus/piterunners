c462fa6790935fc28333a2f6284c84e34d31ebc0
Pontus Gustafsson <pontus.gustafsson@datscha.com>
2021-12-13 01:10:46 +0100

    Minor refactoring

--- client/src/helpers/getPace.js
+++ client/src/helpers/getPace.js
@@ -1,11 +1,7 @@
-const getPace = (hours, minutes, seconds, distance) => {
-  const total =
-    parseInt(hours) * 3600 + parseInt(minutes) * 60 + parseInt(seconds);
-
-  const pace = total / parseFloat(distance.replace(',', '.'));
-
+const getPace = (h, m, s, d) => {
+  const total = parseInt(h) * 3600 + parseInt(m) * 60 + parseInt(s);
+  const pace = total / parseFloat(d.replace(',', '.'));
   const min = parseInt(pace / 60);
-
   const sec = Math.round(pace % 60);
 
   return min + "'" + (sec > 10 ? sec : '0' + sec) + '/km';
