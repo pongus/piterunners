f5afb597ab9a5aa95fd6ea713fa072de832c9567
Pontus Gustafsson <pontus.gustafsson@datscha.com>
2021-12-13 01:10:15 +0100

    Added one more loader

--- client/src/athlete/Results.js
+++ client/src/athlete/Results.js
@@ -3,8 +3,10 @@ import { string } from 'prop-types';
 import { Link } from 'react-router-dom';
 import resultsApi from '../apis/resultsApi';
 import getPace from '../helpers/getPace';
+import Loader from '../common/Loader';
 
 const AthleteResults = ({ athleteId: id }) => {
+  const [isLoading, setIsLoading] = useState(true);
   const [results, setResults] = useState([]);
 
   useEffect(() => {
@@ -12,49 +14,56 @@ const AthleteResults = ({ athleteId: id }) => {
       .then((response) => {
         setResults(response.data.data);
       })
+      .then((data) => {
+        setIsLoading(false);
+      })
       .catch((error) => {
         console.error(error);
       });
   }, [id, setResults]);
 
-  return results.length > 0 ? (
-    <table>
-      <thead>
-        <tr>
-          <th>Datum</th>
-          <th>Tävling</th>
-          <th>Distans</th>
-          <th>Tid</th>
-          <th>Tempo</th>
-        </tr>
-      </thead>
-      <tbody>
-        {results.map((result) => (
-          <tr key={result.id}>
-            <td>{new Date(result.date).toLocaleDateString('sv-SE')}</td>
-            <td>
-              <Link to={`/events/${result.id}`}>{result.name}</Link>
-            </td>
-            <td>
-              {result.distance} {result.unit}
-            </td>
-            <td>
-              {result.hours}:{result.minutes}:{result.seconds}
-            </td>
-            <td>
-              {getPace(
-                result.hours,
-                result.minutes,
-                result.seconds,
-                result.distance
-              )}
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  ) : (
-    <p>Inga resultat</p>
+  return (
+    <>
+      <Loader isLoading={isLoading} />
+
+      {results.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Datum</th>
+              <th>Tävling</th>
+              <th>Distans</th>
+              <th>Tid</th>
+              <th>Tempo</th>
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((result) => (
+              <tr key={result.id}>
+                <td>{new Date(result.date).toLocaleDateString('sv-SE')}</td>
+                <td>
+                  <Link to={`/events/${result.id}`}>{result.name}</Link>
+                </td>
+                <td>
+                  {result.distance} {result.unit}
+                </td>
+                <td>
+                  {result.hours}:{result.minutes}:{result.seconds}
+                </td>
+                <td>
+                  {getPace(
+                    result.hours,
+                    result.minutes,
+                    result.seconds,
+                    result.distance
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
   );
 };
 
